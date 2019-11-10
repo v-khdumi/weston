cebe68baf6ca822ace4e303bc7b50395e05c0884
Josh <josh.tait3@gmail.com>
2019-11-10 18:09:54 +0000

    setup localhost cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Weston
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
